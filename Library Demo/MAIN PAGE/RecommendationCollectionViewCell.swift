0d497e679215f9ad294f929759228111a1dbb6eb
eva <melody88612@gmail.com>
2020-07-05 23:39:36 +0800

    Initial Commit

---
+++ Library Demo/MAIN PAGE/RecommendationCollectionViewCell.swift
@@ -0,0 +1,21 @@
+//
+//  RecommendationCollectionViewCell.swift
+//  Library Demo
+//
+//  Created by mis on 2020/5/26.
+//  Copyright © 2020 Developers Academy. All rights reserved.
+//
+
+import UIKit
+
+class RecommendationCollectionViewCell: UICollectionViewCell {
+    @IBOutlet weak var ImageView: UIImageView!
+    var image: UIImage! {
+        didSet {
+            self.ImageView.image = image
+            self.setNeedsLayout()
+        }
+    }
+    
+    
+}
